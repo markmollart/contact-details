fcefaddf5ed38be86b91347be11767cd2b0ec8c9
Luke Rollans <hello@lukerollans.me>
2013-10-07 17:15:20 +0800

    Added handler for display admin page

--- lib/class.contact-details.php
+++ lib/class.contact-details.php
@@ -7,6 +7,26 @@ final class BirdBrain_Contact_Details {
 	public function __construct () {
 	
 		// Build up hooks
+		add_action( 'admin_menu', array( __CLASS__, 'admin_menu' ) );
+	
+	}
+	
+	public static function admin_menu () {
+	
+		// Add menu page which allows the user to specify which post types to work with
+		add_options_page( 'Contact Details', 'Contact Details', 'manage_options', 'birdbrain-contact-details', array( __CLASS__, '_options_page' )  );
+	
+	}
+	
+	public static function _options_page () {
+	
+		// Render options in-line with WordPress core styling
+		echo '<div class="wrap" id="post-navigator-settings">';
+		
+		echo '	<div class="icon32" id="icon-options-general"><br></div>';
+		echo '	<h2>Contact Details</h2>';
+		
+		echo '</div>';
 	
 	}
 
